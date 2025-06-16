32baa727231a514035d85a7764d1f2b35684a0d0
Ajin V <ajinvk2000@gmail.com>
2025-06-16 12:19:36 +0530

    Initial commit

--- src/component/utils/CommonCard.tsx
+++ src/component/utils/CommonCard.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
-import { motion } from 'framer-motion';
 
 interface CommonImageCardProps {
   title: string;
@@ -10,25 +9,19 @@ interface CommonImageCardProps {
 
 const CommonImageCard = ({ title, image, onClick, alt }: CommonImageCardProps) => {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 30 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5, ease: "easeOut" }}
-    >
-      <Card className="card-box" onClick={onClick}>
-        <CardMedia
-          component="img"
-          image={image}
-          alt={alt || title}
-          className="media-image"
-        />
-        <CardContent>
-          <Typography variant="h6" fontWeight={700} sx={{ mb: -2 }}>
-            {title}
-          </Typography>
-        </CardContent>
-      </Card>
-    </motion.div>
+    <Card className="card-box" onClick={onClick}>
+      <CardMedia
+        component="img"
+        image={image}
+        alt={alt || title}
+        className="media-image"
+      />
+      <CardContent>
+        <Typography variant="h6" fontWeight={700} sx={{ mb: -2 }}>
+          {title}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
